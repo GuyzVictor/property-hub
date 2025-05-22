e6c3aab8adbb995cbfc5f1a05318725b695fdfe0
GuyzVictor <umeobichukwudi2004@gmail.com>
2025-05-22 08:34:15 -0600

    Completed property model.py

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Property(models.Model):
+    Address = models.CharField(max_length=200)
+    zip_code = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    city = models.CharField(max_length=200)  # California, USA
+    bedroom = models.IntegerField()
+    bathroom = models.IntegerField()
+    emailaddress = models.CharField(max_length=200)
+    clients_name = models.CharField(max_length=200)
+    subject = models.CharField(max_length=200)
+    message = models.CharField(max_lenght=500)
